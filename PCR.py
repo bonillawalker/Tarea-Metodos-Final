05da852eb183f643aeb9b78e25291761b2478d2c
Felipe   Bonilla Walker <UANDES\f.bonilla11@fisicaez12202l.ad.uniandes.edu.co>
2018-10-29 12:39:19 -0500

    Se saca la matriz de cov

--- PCR.py
+++ PCR.py
@@ -8,3 +8,20 @@ Diagnostic = np.genfromtxt('WDBC.dat', delimiter=',', usecols=1, dtype=str) #Car
 Id = Data[:,0] 								    #Saqueme los Id
 X = Data[:,2:]                                                              #Saqueme solo los datos numericos
 
+n = np.size(X,1)							    #Asigno el valor de n como el tamano de x
+Matriz = np.zeros((n,n))                                                    #Matriz de ceros para ir guardando los valores
+
+mu = np.mean(X,0)                                                           #Calculo el promedio de las columnas
+dif = X-mu 								    #Diferencia de cada dato con su promedio. Le resto a cada valor su promedio
+
+
+for i in range(n):							    #Creo ciclo que me recorra la matriz
+    for j in range(n):
+        Matriz[i,j] = np.mean(dif[:,i]*dif[:,j])                            #Para cada valor calcular cada covarianza
+
+
+print('Matriz de Covarianza:') 				            	    #Imprima la matriz de covarianza para revisar
+print(Matriz)
+print(Matriz, np.cov(X.T), sep='\n'*2)					    #Con numpy puedo verificar si los numeros de mi matriz de cov estan bien
+print('\n')
+
