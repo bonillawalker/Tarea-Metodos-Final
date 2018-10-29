c5fb0945fb2f25c7b8a59dc8ea3c3a7a27a2254c
Felipe   Bonilla Walker <UANDES\f.bonilla11@fisicaez12202l.ad.uniandes.edu.co>
2018-10-29 14:52:58 -0500

    Lee los archivos y crea la primera grafica

--- Fourier.py
+++ Fourier.py
@@ -1,2 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Cargar datos de las dos senales
+S = np.genfromtxt('signal.dat', delimiter=',')            
+I = np.genfromtxt('incompletos.dat', delimiter=',')	  
+
+######################## Grafica de senal
+plt.figure()						  
+plt.plot(S[:,0], S[:,1], '.')                             
+plt.xlabel('x')						  
+plt.ylabel('y')
+plt.savefig('BonillaWFelipe_signal.pdf')		  
