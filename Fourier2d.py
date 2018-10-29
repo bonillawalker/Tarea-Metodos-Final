58423b5076c7c036f51e49e948a7d3e8713c4ef5
Felipe   Bonilla Walker <UANDES\f.bonilla11@fisicaez12202l.ad.uniandes.edu.co>
2018-10-29 13:38:51 -0500

    Grafica de la transformada

--- Fourier2d.py
+++ Fourier2d.py
@@ -1,2 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.image as img                                #Con esto puedo leer la imagen.png
+from scipy.fftpack import fft2      
+         	      
+
+I = img.imread('arbol.png')                                   
+
+I_FT = fft2(I) 						      
+
+plt.figure(figsize=(12,6))                                    
+plt.subplot(1,2,1)					      
+plt.imshow(((np.abs(I_FT))))				     
+plt.colorbar()						      
+plt.grid()						      
+
+
+plt.subplot(1,2,2)					      
+plt.imshow((np.log( np.abs(I_FT))[0:80,0:80]) )              
+plt.grid()						      
+plt.colorbar()						     
+
+plt.savefig('BonillaWFelipe_FT2D.pdf')			      
+
